3ca2d0889d317f798713b83bfc3fa13a03d3f44a
Moksha Chowdary Guntupalli <mokshachowdaryg@usf.edu>
2024-02-28 14:34:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 3, 62, 5, 16, 4, 10 };
+
+        Console.WriteLine("Original Array:");
+        PrintArray(array);
+
+        BubbleSort(array);
+
+        Console.WriteLine("\nSorted Array:");
+        PrintArray(array);
+    }
+
+    static void BubbleSort(int[] arr)
+    {
+        int n = arr.Length;
+        bool swapped;
+
+        do
+        {
+            swapped = false;
+
+            for (int i = 1; i < n; i++)
+            {
+                if (arr[i - 1] > arr[i])
+                {
+                    // Swap arr[i-1] and arr[i]
+                    int temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+
+                    swapped = true;
+                }
+            }
+
+            // After each pass, the largest element is guaranteed to be at the end,
+            // so we can reduce the size of the array to be considered in the next pass.
+            n--;
+        } while (swapped);
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (var element in arr)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
